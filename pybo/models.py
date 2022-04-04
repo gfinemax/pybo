0b899d91c3b92cbb75f27774f484612f7ea586fa
gfinemax <gfinemax@gmail.com>
2022-04-04 16:28:31 +0900

    모델속성 구상하기

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Question(models.Model):
+    suject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+class Answer(models.Model):
+    guestion = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
 # Create your models here.
