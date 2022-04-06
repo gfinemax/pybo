37d382a51755f14d2a9ddbe630c857180057252b
gfinemax <gfinemax@gmail.com>
2022-04-06 12:46:05 +0900

    URL별칭으로 URL하드코딩 문제해결하기

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail),
+    path('', views.index, name='index'), # url 별칭부여
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
